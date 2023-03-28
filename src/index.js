286e92c7aa330afbfd502610134a83a8ba6169be
unknown <armanazad.sakib@gmail.com>
2023-03-29 00:11:29 +0600

    configure Dockerfile & docker-compose file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import './scss/custom.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <h1>Boostrap 5.2 installed & working</h1>
   </React.StrictMode>
 );
